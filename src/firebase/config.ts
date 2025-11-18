21cbf4a6cbd1c71dbf3b842d175a101535b39e43
bernardo galva <beganetflix@gmail.com>
2025-11-18 07:00:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8503105234-550d0",
+  "appId": "1:192229270466:web:58ad4b19fc88e184e1ec5b",
+  "apiKey": "AIzaSyAei6oYH-MlOe0H09lJD7uTInAxbv2149w",
+  "authDomain": "studio-8503105234-550d0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "192229270466"
+};
